f30a7e4623ff2b4b913492fb4aba3218f664981b
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2024-04-09 11:41:50 +0100

    info stage

--- voting/Voting/__init__.py
+++ voting/Voting/__init__.py
@@ -1,4 +1,5 @@
 from otree.api import *
+import random
 
 doc = """
 Voting Game
@@ -8,35 +9,66 @@ class C(BaseConstants):
     NAME_IN_URL = 'Voting'
     PLAYERS_PER_GROUP = 3
     NUM_ROUNDS = 10
-    AMOUNT_SHARED_IF_A = cu(100)  # Amount shared if option A wins
-    AMOUNT_SHARED_IF_B = cu(20)   # Amount shared if option B wins
-    CHOICES = ['A', 'B']  # Voting options
+    AMOUNT_SHARED_IF_A = cu(100)
+    AMOUNT_SHARED_IF_B = cu(20)
+    CHOICES = ['A', 'B']
+    STATES = ['A', 'B']
+    CATEGORIES = ['h', 'l']
+    REWARDS = ['r', 'b']
+
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        for group in self.get_groups():
+            group.state = random.choice(C.STATES)
+
 
 class Group(BaseGroup):
+    state = models.StringField()
+
+    def set_categories_and_rewards(self):
+        for player in self.get_players():
+            player.category = random.choice(C.CATEGORIES)
+            # A状态下的分配
+            if self.state == 'A':
+                if player.category == 'h':
+                    player.reward = random.choices(C.REWARDS, weights=(5, 3), k=1)[0]
+                else:
+                    player.reward = random.choices(C.REWARDS, weights=(4, 3), k=1)[0]
+            # B状态下的分配
+            else:
+                if player.category == 'h':
+                    player.reward = random.choices(C.REWARDS, weights=(3, 5), k=1)[0]
+                else:
+                    player.reward = random.choices(C.REWARDS, weights=(3, 4), k=1)[0]
+
     def set_payoffs(self):
+        # 根据状态投票的结果分配payoff
         votes = [p.vote for p in self.get_players()]
-        if votes.count('A') >= 2:
+        if self.state == 'A' and votes.count('A') >= 2 or self.state == 'B' and votes.count('B') >= 2:
             payoff = C.AMOUNT_SHARED_IF_A
         else:
             payoff = C.AMOUNT_SHARED_IF_B
         for p in self.get_players():
             p.payoff = payoff
 
+
 class Player(BasePlayer):
     vote = models.StringField(choices=C.CHOICES, label="Please choose A or B")
+    category = models.StringField()
+    reward = models.StringField()
 
-    def chat_nickname(self):
-        return 'Voter {}'.format(self.id_in_group)
+class Info(Page):
 
-    def chat_room(self):
-        if self.id_in_group in [1, 3]:
-            return '{}-{}'.format(C.NAME_IN_URL, self.group.pk)
-        return None
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {
+            'state': player.group.state,
+            'category': player.category,
+            'reward': player.reward
+        }
 
-# PAGES
+
 class Chat(Page):
     @staticmethod
     def is_displayed(player):
@@ -55,4 +87,4 @@ class ResultsWaitPage(WaitPage):
 class Results(Page):
     pass
 
-page_sequence = [Chat, Voting, ResultsWaitPage, Results]
+page_sequence = [Info, Chat, Voting, ResultsWaitPage, Results]
