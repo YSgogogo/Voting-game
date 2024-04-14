9861063074b8dad898f6fce9009bbd86df4a6914
Yi Shi <122290415+YSgogogo@users.noreply.github.com>
2024-04-14 01:28:27 +0100

    version 1.0

--- voting/Voting/__init__.py
+++ voting/Voting/__init__.py
@@ -386,16 +386,96 @@ class Main_Instructions(Page):
 class Info(Page):
     @staticmethod
     def vars_for_template(player):
+        if player.qualities == 'l':
+            quality_display = "Urn (2)"
+        else:
+            quality_display = "Urn (1)"
+
+        if player.signals == 'r':
+            player_signal_color = "red"
+            player_signals_display = "RED"
+        else:  # 'b'
+            player_signal_color = "blue"
+            player_signals_display = "BLUE"
+
+        player_signal_style = f"height: 1.2em; width: 1.2em; background-color: {player_signal_color}; border-radius: 50%; display: inline-block; margin: 0 5px;"
+        player_text_color_style = f"color: {player_signal_color}; font-weight: bold;"
+
+        other_signals_info = []
+        for p in player.group.get_players():
+            if p.id_in_group != player.id_in_group:
+                if p.signals == 'r':
+                    signal_color = "red"
+                    signals_display = "RED"
+                else:  # 'b'
+                    signal_color = "blue"
+                    signals_display = "BLUE"
+
+                other_signals_info.append({
+                    'player_id': p.id_in_group,
+                    'signals_display': signals_display,
+                    'signal_style': f"height: 1.2em; width: 1.2em; background-color: {signal_color}; border-radius: 50%; display: inline-block; margin: 0 5px;",
+                    'text_color_style': f"color: {signal_color}; font-weight: bold;",
+                })
+
         return dict(
-            quality=player.qualities,
-            signal=player.signals
+            quality=quality_display,
+            player_signals_display=player_signals_display,
+            player_signal_style=player_signal_style,
+            player_text_color_style=player_text_color_style,
+            other_signals_info=other_signals_info,
         )
+
+
 
 
 
 class Ranking(Page):
     form_model = 'player'
     form_fields = ['ranking']
+
+    @staticmethod
+    def vars_for_template(player):
+        if player.qualities == 'l':
+            quality_display = "Urn (2)"
+        else:
+            quality_display = "Urn (1)"
+
+        if player.signals == 'r':
+            player_signal_color = "red"
+            player_signals_display = "RED"
+        else:  # 'b'
+            player_signal_color = "blue"
+            player_signals_display = "BLUE"
+
+        player_signal_style = f"height: 1.2em; width: 1.2em; background-color: {player_signal_color}; border-radius: 50%; display: inline-block; margin: 0 5px;"
+        player_text_color_style = f"color: {player_signal_color}; font-weight: bold;"
+
+        other_signals_info = []
+        for p in player.group.get_players():
+            if p.id_in_group != player.id_in_group:
+                if p.signals == 'r':
+                    signal_color = "red"
+                    signals_display = "RED"
+                else:  # 'b'
+                    signal_color = "blue"
+                    signals_display = "BLUE"
+
+                other_signals_info.append({
+                    'player_id': p.id_in_group,
+                    'signals_display': signals_display,
+                    'signal_style': f"height: 1.2em; width: 1.2em; background-color: {signal_color}; border-radius: 50%; display: inline-block; margin: 0 5px;",
+                    'text_color_style': f"color: {signal_color}; font-weight: bold;",
+                })
+
+        return dict(
+            quality=quality_display,
+            player_signals_display=player_signals_display,
+            player_signal_style=player_signal_style,
+            player_text_color_style=player_text_color_style,
+            other_signals_info=other_signals_info,
+        )
+
 
     def before_next_page(player, timeout_happened):
 
@@ -444,17 +524,34 @@ class Chat(Page):
 
         for participant_id in chat_participants_ids:
             participant = next(p for p in player.group.get_players() if p.id_in_group == participant_id)
+            # 确定信号表示
+            if participant.signals == 'r':
+                player_signal_color = "red"
+                player_signals_display = "RED"
+            else:   # b
+                player_signal_color = "blue"
+                player_signals_display = "BLUE"
+
+            player_signal_style = f"height: 1.2em; width: 1.2em; background-color: {player_signal_color}; border-radius: 50%; display: inline-block; margin: 0 5px;"
+            player_text_color_style = f"color: {player_signal_color}; font-weight: bold;"
+
+            if participant.qualities == 'l':
+                quality_representation = "Urn (2)"
+            else:
+                quality_representation = "Urn (1)"
+
             participants_info.append({
                 'id_in_group': participant.id_in_group,
-                'quality': participant.qualities,
-                'signal': participant.signals,
+                'quality_representation': quality_representation,
+                'player_signal_style': player_signal_style,
+                'player_signals_display': player_signals_display,
+                'player_text_color_style': player_text_color_style,
                 'is_self': participant.id_in_group == player.id_in_group
             })
 
         participants_info = sorted(participants_info, key=lambda x: not x['is_self'])
 
         return {
-            'state': player.state,
             'participants_info': participants_info
         }
 
